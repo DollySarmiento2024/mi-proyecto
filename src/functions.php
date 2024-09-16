49cd2c1e7936e22b92bae9cb239447d41bb8a587
DollySarmiento2024 <dosaqu@floridauniversitaria.es>
2024-09-17 00:57:38 +0200

    Se agrega función saludo_personalizado en functions.php

--- src/functions.php
+++ src/functions.php
@@ -4,4 +4,8 @@ function saludo(){
     return "Hola, bienvenido a Despliegue de aplicaciones web";
 }
 
+function saludo_personalizado($nombre){
+    return "Hola " . $nombre . ", bienvenido a Despliegue de aplicaciones web";
+}
+
 ?>
